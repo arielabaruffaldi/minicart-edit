b3575064dc1feebd4ed42228300b4c7841c0522f
arielabaruffaldi <baruffaldiariela@gmail.com>
2021-12-22 21:39:55 -0300

    swiper

--- react/components/ProductQuantity/ProductQuantity.tsx
+++ react/components/ProductQuantity/ProductQuantity.tsx
@@ -1,16 +1,27 @@
 import React, { useContext } from 'react'
 import { ModalContext } from '../../store/context/ModalContext'
+import { useCssHandles } from 'vtex.css-handles'
+
+const CSS_HANDLES = ['ProductQuantity', 'ProductQuantity--button', 'ProductQuantity--button-add', 'ProductQuantity--button-remove', 'ProductQuantity--quantity']
 
 const ProductQuantity = () => {
     const { state, dispatch } = useContext(ModalContext)
+    const handles = useCssHandles(CSS_HANDLES)
 
     return (
-        <>
-            <button onClick={()=> state.quantity > 1 && dispatch({type: 'SET_QUANTITY', payload: state.quantity - 1})}> - </button>
-            <span>{state.quantity}</span>
-            <button onClick={()=>dispatch({type: 'SET_QUANTITY', payload: state.quantity + 1})}> + </button>
-
-        </>
+        <div className={handles.ProductQuantity}>
+            <button
+                className={`${handles['ProductQuantity--button']} ${handles['ProductQuantity--button-remove']}`}
+                onClick={() => state.quantity > 1 && dispatch({ type: 'SET_QUANTITY', payload: state.quantity - 1 })
+                }> -
+            </button>
+            <span className={handles['ProductQuantity--quantity']}>{state.quantity}</span>
+            <button
+                className={`${handles['ProductQuantity--button']} ${handles['ProductQuantity--button-add']}`}
+                onClick={() => dispatch({ type: 'SET_QUANTITY', payload: state.quantity + 1 })}
+            > +
+            </button>
+        </div>
     )
 }
 
--- react/components/Slider/Slider.tsx
+++ react/components/Slider/Slider.tsx
@@ -1,38 +1,58 @@
 import React, { useContext } from 'react';
-import { SliderLayout } from 'vtex.slider-layout'
+// import { SliderLayout } from 'vtex.slider-layout'
+// import { ModalContext } from '../../store/context/ModalContext';
+// import { Slider } from 'vtex.store-components'
+
+import SwiperCore, { Thumbs, Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { ModalContext } from '../../store/context/ModalContext';
 
+SwiperCore.use([Thumbs, Navigation, Pagination])
 
-const Slider = () => {
+
+const Slider: any = () => {
     const { state } = useContext(ModalContext)
 
     return (
-        <SliderLayout
-            itemsPerPage={{
-                desktop: 2,
-                phone: 4,
-            }}
-            showNavigationArrows="always"
-            centerMode="to-the-left"
-            centerModeSlidesGap={15}
-            showPaginationDots="never"
-            fullWidth={false}
-            infinite={false}
-            blockClass="home-bottom-cats"
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
         >
-            {state.images.map((image: any, index: number) => {
-                return (
-                    <img key={index} src={image} alt={image} />
-                )
-            })}
-        </SliderLayout>
-        /*  {
-             state.images &&
-             state.images.map((image: any, index: number) => {
-                 if (index === state.activeSku?.imageUrls.length - 1) return
-                 return <img key={index} src={image} alt={image} />
-             })
-         } */
+            {state.images.map((image: any, index: number) =>
+                <SwiperSlide
+                    key={`slider-${index}`}
+                >
+                    <img src={image} alt={image} />
+                </SwiperSlide>
+
+            )}
+
+        </Swiper>
+
+
+        /*      <SliderLayout
+                 itemsPerPage={{
+                     desktop: 2,
+                     phone: 4,
+                 }}
+                 showNavigationArrows="always"
+                 centerMode="to-the-left"
+                 centerModeSlidesGap={15}
+                 showPaginationDots="never"
+                 fullWidth={false}
+                 infinite={false}
+                 blockClass="home-bottom-cats"
+             >
+                 {state.images.map((image: any, index: number) => {
+                     return (
+                         <img key={index} src={image} alt={image} />
+                     )
+                 })}
+             </SliderLayout>
+      */
+
     )
 }
 
